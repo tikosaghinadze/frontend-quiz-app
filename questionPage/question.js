f12c7f1db29c9a9636fe5e1a6abbd07804c3ed4d
tikosaghinadze <teasaghinadze20@gmail.com>
2024-02-06 22:55:35 +0400

    use local storage to save elements and after for to take elemmets

---
+++ questionPage/question.js
@@ -0,0 +1,4 @@
+const savedHtmlApi = localStorage.getItem("htmlApi");
+const savedCssApi = localStorage.getItem("cssApi");
+const savedJsApi = localStorage.getItem("jsApi");
+const savedAccesApi = localStorage.getItem("accesApi");
--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const accesBtn = document.getElementById("accessibility");
 
 htmlBtn.addEventListener("click", async () => {
   try {
-    const response = await fetch();
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=22"
+    );
     const htmlApi = await response.json();
+    localStorage.setItem("htmlApi", htmlApi);
+    console.log(htmlApi);
   } catch (error) {
     console.log(error);
   }
@@ -14,8 +18,11 @@ htmlBtn.addEventListener("click", async () => {
 
 cssBtn.addEventListener("click", async () => {
   try {
-    const response = await fetch();
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=23"
+    );
     const cssApi = await response.json();
+    localStorage.setItem("cssApi", cssApi);
   } catch (error) {
     console.log(error);
   }
@@ -23,8 +30,11 @@ cssBtn.addEventListener("click", async () => {
 
 jsBtn.addEventListener("click", async () => {
   try {
-    const response = await fetch();
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=11"
+    );
     const jsApi = await response.json();
+    localStorage.setItem("jsApi", jsApi);
   } catch (error) {
     console.log(error);
   }
@@ -32,8 +42,11 @@ jsBtn.addEventListener("click", async () => {
 
 accesBtn.addEventListener("click", async () => {
   try {
-    const response = await fetch();
+    const response = await fetch(
+      "https://opentdb.com/api.php?amount=10&category=18"
+    );
     const accesApi = await response.json();
+    localStorage.setItem("accesApi", accesApi);
   } catch (error) {
     console.log(error);
   }
